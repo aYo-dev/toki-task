3a1ade244c49ab26ccc1c90e41fa03cb0c557704
Asen Yovchev <ayovcheff@gmail.com>
2022-10-07 18:33:10 +0300

    create api service

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -3,3 +3,16 @@ export interface AccountInfo {
   owner: string,
   meteringPoints: string[]
 }
+
+export interface UsageResponse {
+  timestamp: number;
+  kwh: number;
+}
+
+export type MeteringPoints = '1234' | '5678';
+
+export interface Date {
+  year: number;
+  month: number;
+  day: number;
+} 
---
+++ services/api.service.ts
@@ -0,0 +1,29 @@
+import {Storage} from '@google-cloud/storage';
+import { Date, MeteringPoints, UsageResponse } from '../interfaces';
+
+const bucketName = 'toki-take-home.appspot.com';
+const storage = new Storage({keyFilename: 'key.json'});
+const db = storage.bucket(bucketName);
+
+/**
+ * convert jsonl string to json array
+ * @param jsonlString - jsonl file in string format
+ * @returns json array
+ */
+export const parseJSONL = (jsonlString: string): Record<string, any>[] => 
+  jsonlString.split(/\n/)
+    .map(line => JSON.parse(line)) as UsageResponse[];
+
+// dev purposes
+const getFilesSelfLink = (files: any[]) =>
+  files.map(el => el.metadata.selfLink);
+
+/**
+ * return used electricity for a day in format related to meteringPoinId
+ */
+export const getUsagePerDay = async (meteringPoinId: MeteringPoints, date: Date): Promise<UsageResponse[]> => {
+  const file = await db.file(`usage/${date.year}/${date.month}/${date.day}/${meteringPoinId}.jsonl`)
+    .download();
+  const bufferString = file[0].toString('utf8');
+  return parseJSONL(bufferString) as UsageResponse[];
+};
