1bcd05ef91dc2204f941f4221f7588e3bcd0ce81
Asen Yovchev <ayovcheff@gmail.com>
2022-10-13 13:24:24 +0300

    Edit styles and representation logic

--- components/DataList.tsx
+++ components/DataList.tsx
@@ -1,36 +1,62 @@
-import * as React from 'react';
+import { useMemo } from 'react';
 import Paper from '@mui/material/Paper';
-import { List, Typography } from '@mui/material';
+import { Alert, List, Typography } from '@mui/material';
 import { DataCategories } from '../enums';
-import { PriceData, UsageData } from '../interfaces';
+import { DataCategory, PriceData, UsageData } from '../interfaces';
 import PriceListItem from './PriceListItem';
 import UsageListItem from './UsageListItem';
 import { isEmpty } from 'ramda';
 import { isPricesCategory } from '../utils';
+import { nanoid } from 'nanoid';
+
 
 interface ControllersBoxProps {
-  items: PriceData[] | UsageData[];
+  items: DataCategory;
   category: DataCategories;
 }
 
+const NoData = () => (
+  <Alert variant="outlined" severity="info">
+    Data is missing for this day.
+  </Alert>
+);
+
+// items should have strict type
+const getDailyAmount = (items: any[], key: string): number =>
+  items.reduce((acc, item) => acc + item[key], 0);  
+
 export default function DataList({items, category}: ControllersBoxProps) {
   const noItems = isEmpty(items);
-  const listItems = React.useMemo(() => {
+  const listItems = useMemo(() => {
     if(isPricesCategory(category)){
       return items.map((el, index) => 
-        <PriceListItem priceData={el as PriceData} hasDivider={index !== 0}/>);
+        <PriceListItem key={nanoid()} priceData={el as PriceData} hasDivider={index !== 0}/>);
     }
 
     return items.map((el, index) => 
-      <UsageListItem priceData={el as UsageData} hasDivider={index !== 0}/>);
+      <UsageListItem key={nanoid()} priceData={el as UsageData} hasDivider={index !== 0}/>);
   }, [category, items]);
 
+  const dailyAmount = useMemo(() => {
+    const result = getDailyAmount(items, 'kwh');
+    // we don't want very long float number
+    return result.toFixed(3);
+  }, [items])
+
   return (
-    <Paper sx={{marginTop: 4}}>
-      <Typography>{category} for the day</Typography>
+    <Paper sx={{marginTop: 4, padding: 2}}>
+      <Typography 
+        paddingRight={2}
+        paddingTop={2}
+        fontWeight='bold'
+        textAlign='right'
+        borderBottom="1px solid"
+      >
+          Total amount: {dailyAmount}
+      </Typography>
       <List>
         {!noItems && listItems}
-        {noItems && <p>Data is missing for this day</p>}
+        {noItems && <NoData />}
       </List>
     </Paper>
   );
